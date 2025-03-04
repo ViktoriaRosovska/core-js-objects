ded0276302300807328abf1b7f7db748eb0a0c29
Viktoria Rosovska <torimeister@gmail.com>
2025-03-04 20:37:16 +0200

    feat: solve sellTickets function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -174,8 +174,13 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let sum = queue[0];
+  for (let i = 0; i < queue.length; i += 1) {
+    if (queue[i] - sum > 0) return false;
+    sum += queue[i];
+  }
+  return true;
 }
 
 /**
