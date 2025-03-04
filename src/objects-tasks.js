5e94bedbc1a85a03f5f4ee14094f0f48f866ec62
Viktoria Rosovska <torimeister@gmail.com>
2025-03-04 19:18:31 +0200

    feat: add solve for makeWord function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -142,8 +142,22 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const map = new Map();
+  let str = '';
+  const entries = Object.entries(lettersObject);
+  for (let i = 0; i < entries.length; i += 1) {
+    const [key, value] = entries[i];
+    for (let j = 0; j < value.length; j += 1) {
+      if (!map.has(value[j])) {
+        map.set(value[j], key);
+      }
+    }
+  }
+  for (let k = 0; k < map.size; k += 1) {
+    str += map.get(k);
+  }
+  return str;
 }
 
 /**
